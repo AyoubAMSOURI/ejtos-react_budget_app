ed682d51e225567ec03ce9cc3f4d03d89cca8526
ayoub amsouri <ayoubamsouri1999@gmail.com>
2023-03-19 15:38:41 +0000

    adding budget and remaing components

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,14 @@
+import { useContext } from "react"
+import { AppContext } from "../context/AppContext"
 
+const Budget = () =>{
+    let {budget} = useContext(AppContext);
+    return(
+        <div className="alert alert-secondary">
+            <span>
+                Budget : {budget} £
+            </span>
+        </div>
+    )
+}
+export default Budget;
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,15 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
+const Remaining = () =>{
+    const {remaining} = useContext(AppContext);
+    const alertType = remaining >= 0 ? 'alert-success' : 'alert-danger' ;
+    return(
+        <div className={`alert ${alertType}`}>
+            <span>
+                Remaining : {remaining} £
+            </span>
+        </div>
+    )
+}
+export default Remaining ;
